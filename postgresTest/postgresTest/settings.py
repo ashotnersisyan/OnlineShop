e9de0ed9bd70f19c9918b5647ee949611f2dceb4
ashotnersisyan <ashotnersisyan1996@gmail.com>
2023-02-13 23:28:47 +0400

    final

--- postgresTest/postgresTest/settings.py
+++ postgresTest/postgresTest/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+9mxm(50(+q$kdx%lq1fhykh!&ub^bn^tfr#u8_-k^lyi%%*8e'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'myproject',
         'USER': 'myprojectuser',
-        'PASSWORD': 'password',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '',
     }
